f1dd96c5123c9ad80198a0c91c6141498064d2a1
Rafael M <github@rafaelmaeuer.de>
2021-02-22 21:37:56 +0100

    add more unit tests for api endpoints

--- test/api.test.js
+++ test/api.test.js
@@ -8,29 +8,31 @@
 */
 
 // Require dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../src/ingest/index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../src/ingest/index');
 
-// Init funcs and vars
-let should = chai.should();
-let expect = chai.expect;
-let token = null;
+// Init functions
+const should = chai.should();
+const expect = chai.expect;
 
 // Use chaiHttp
 chai.use(chaiHttp);
 
-// Define test cases
-
 /*
-* Test the POST /auth/login route
+    AUTH - Authentication services for Eventhub
 */
+
+var accessToken = null;
+var refreshToken = null;
+
 describe('POST /auth/login', () => {
-    it('it should return a user with access token', (done) => {
+    it('swap login credentials for an id-token', (done) => {
         let login = {
             email: process.env.TEST_USER,
             password: process.env.TEST_USER_PW
         }
+
         chai.request(server)
             .post('/auth/login')
             .send(login)
@@ -38,29 +40,146 @@ describe('POST /auth/login', () => {
                 res.should.have.status(200);
                 res.body.should.be.a('object');
                 res.body.should.have.property('expiresIn').to.be.a('number');
+                res.body.should.have.property('expires').to.be.a('string');
                 res.body.should.have.property('token').to.be.a('string');
                 res.body.should.have.property('refreshToken').to.be.a('string');
                 res.body.should.have.property('user').to.be.a('object');
                 res.body.user.should.have.property('user_id').to.be.a('string');
-                res.body.user.should.have.property('email_verified').eql(true);
+                res.body.user.should.have.property('email_verified').to.be.a('boolean');
                 res.body.should.have.property('trace').eql(null);
             done();
-            // Store token for further tests
-            token = res.body.token;
+            // Store tokens for further tests
+            accessToken = res.body.token;
+            refreshToken = res.body.refreshToken;
             });
     });
+});
 
+describe('POST /auth/refresh', () => {
+    it('swap refresh-token for new id-token', (done) => {
+        let refresh = {
+            refreshToken: refreshToken
+        }
+
+        chai.request(server)
+            .post('/auth/refresh')
+            .send(refresh)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('expiresIn').to.be.a('number');
+                res.body.should.have.property('expires').to.be.a('string');
+                res.body.should.have.property('token').to.be.a('string');
+                res.body.should.have.property('refreshToken').to.be.a('string');
+                res.body.should.have.property('user').to.be.a('object');
+                res.body.user.should.have.property('user_id').to.be.a('string');
+                res.body.user.should.have.property('email_verified').to.be.a('boolean');
+                res.body.should.have.property('trace').eql(null);
+            done();
+            // Store new token for further tests
+            accessToken = res.body.token;
+            });
+    });
+});
+
+describe('POST /auth/reset', () => {
+    it('request password reset email', (done) => {
+        let reset = {
+            email: process.env.TEST_USER
+        }
+
+        chai.request(server)
+            .post('/auth/reset')
+            .send(reset)
+            .end((err, res) => {
+                res.should.have.status(200);
+            done();
+            });
+    });
 });
 
 /*
-* Test the GET /subscriptions route
+    EVENTS - Manage events
 */
 
+describe('POST /events/v1', () => {
+    it('publish a new event', (done) => {
+        let body = {
+            "event": "de.ard.eventhub.v1.radio.track.playing",
+            "type": "music",
+            "start": "2020-01-19T06:00:00+01:00",
+            "title": "Song name",
+            "serviceIds": [
+                "284680",
+                "284700"
+            ],
+            "playlistItemId": "swr3-5678"
+        }
+
+        chai.request(server)
+            .post('/events/v1')
+            .set('Authorization', 'Bearer ' + accessToken)
+            .send(body)
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.be.a('object');
+                res.body.should.have.property('topics').to.be.a('object');
+                res.body.should.have.property('message').to.be.a('object');
+                res.body.should.have.property('trace').eql(null);
+            done();
+            });
+    });
+});
+
+/*
+    SUBSCRIPTIONS - Access to subscription management
+*/
+
+var subscriptionName = null;
+
+describe('POST /subscriptions', () => {
+    it('add a new subscription to this user', (done) => {
+        let body = {
+            "type": "PUBSUB",
+            "method": "PUSH",
+            "url": "https://example.com/my/webhook/for/this/subscription",
+            "contact": "my-emergency-and-notifications-contact@ard.de",
+            "topic": "dev"
+        }
+
+        chai.request(server)
+            .post('/subscriptions')
+            .set('Authorization', 'Bearer ' + accessToken)
+            .send(body)
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.be.a('object');
+                res.body.to.have.all.keys(
+                    'type', 
+                    'method', 
+                    'name', 
+                    'path', 
+                    'url', 
+                    'topic',
+                    'ackDeadlineSeconds',
+                    'retainAckedMessages',
+                    'retryPolicy',
+                    'serviceAccount',
+                    'labels',
+                    'created',
+                    'contact',
+                    'owner'
+                    )
+            done();
+        });
+    });
+});
+
 describe('GET /subscriptions', () => {
-    it('it should GET all subscriptions for this user', (done) => {
+    it('list all subscriptions for this user', (done) => {
       chai.request(server)
             .get('/subscriptions')
-            .set('Authorization', 'Bearer ' + token)
+            .set('Authorization', 'Bearer ' + accessToken)
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
@@ -80,7 +199,42 @@ describe('GET /subscriptions', () => {
                     'contact',
                     'owner'
                     ))
+                // Store subscription name for further tests
+                subscriptionName = res.body.name;
             done();
         });
     });
 });
+
+describe('GET /subscriptions/{name}', () => {
+    it('get details about single subscription from this user', (done) => {
+      chai.request(server)
+            .get('/subscriptions/' + subscriptionName)
+            .set('Authorization', 'Bearer ' + accessToken)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.to.have.all.keys(
+                    'type', 
+                    'method', 
+                    'name', 
+                    'path', 
+                    'url', 
+                    'topic',
+                    'ackDeadlineSeconds',
+                    'retainAckedMessages',
+                    'retryPolicy',
+                    'serviceAccount',
+                    'labels',
+                    'created',
+                    'contact',
+                    'owner'
+                    )
+            done();
+        });
+    });
+});
+
+/*
+    TOPICS - Access to topics details
+*/
