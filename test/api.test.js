c7b33ce5d4f7c20301655d0545c377efc732ea3d
Rafael M <github@rafaelmaeuer.de>
2021-02-24 12:16:15 +0100

    refactoring: move redundant code to functions, add type checks

--- test/api.test.js
+++ test/api.test.js
@@ -26,27 +26,31 @@ chai.use(chaiHttp);
 var accessToken;
 var refreshToken;
 
+function testAuthKeys(body) {
+    body.should.be.a('object');
+    body.should.have.property('expiresIn').to.be.a('number');
+    body.should.have.property('expires').to.be.a('string');
+    body.should.have.property('token').to.be.a('string');
+    body.should.have.property('refreshToken').to.be.a('string');
+    body.should.have.property('user').to.be.a('object');
+    body.user.should.have.property('user_id').to.be.a('string');
+    body.user.should.have.property('email_verified').to.be.a('boolean');
+    body.should.have.property('trace').eql(null);
+}
+
 describe('POST /auth/login', () => {
     it('swap login credentials for an id-token', (done) => {
-        let login = {
+        let loginRequest = {
             email: process.env.TEST_USER,
             password: process.env.TEST_USER_PW
         }
 
         chai.request(server)
             .post('/auth/login')
-            .send(login)
+            .send(loginRequest)
             .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('expiresIn').to.be.a('number');
-                res.body.should.have.property('expires').to.be.a('string');
-                res.body.should.have.property('token').to.be.a('string');
-                res.body.should.have.property('refreshToken').to.be.a('string');
-                res.body.should.have.property('user').to.be.a('object');
-                res.body.user.should.have.property('user_id').to.be.a('string');
-                res.body.user.should.have.property('email_verified').to.be.a('boolean');
-                //res.body.should.have.property('trace').eql(null);
+                testAuthKeys(res.body);
             done();
             // Store tokens for further tests
             accessToken = res.body.token;
@@ -57,24 +61,16 @@ describe('POST /auth/login', () => {
 
 describe('POST /auth/refresh', () => {
     it('swap refresh-token for new id-token', (done) => {
-        let refresh = {
+        let refreshRequest = {
             refreshToken: refreshToken
         }
 
         chai.request(server)
             .post('/auth/refresh')
-            .send(refresh)
+            .send(refreshRequest)
             .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('expiresIn').to.be.a('number');
-                res.body.should.have.property('expires').to.be.a('string');
-                res.body.should.have.property('token').to.be.a('string');
-                res.body.should.have.property('refreshToken').to.be.a('string');
-                res.body.should.have.property('user').to.be.a('object');
-                res.body.user.should.have.property('user_id').to.be.a('string');
-                res.body.user.should.have.property('email_verified').to.be.a('boolean');
-                //res.body.should.have.property('trace').eql(null);
+                testAuthKeys(res.body);
             done();
             // Store new token for further tests
             accessToken = res.body.token;
@@ -82,15 +78,16 @@ describe('POST /auth/refresh', () => {
     });
 });
 
+// 🚨 firebase limit is 150 requests per day 🚨
 describe('POST /auth/reset', () => {
     it('request password reset email', (done) => {
-        let reset = {
+        let resetRequest = {
             email: process.env.TEST_USER
         }
 
         chai.request(server)
             .post('/auth/reset')
-            .send(reset)
+            .send(resetRequest)
             .end((err, res) => {
                 res.should.have.status(200);
             done();
@@ -102,9 +99,16 @@ describe('POST /auth/reset', () => {
     EVENTS - Manage events
 */
 
+function testEventKeys(body) {
+    body.should.be.a('object');
+    body.should.have.property('topics').to.be.a('object');
+    body.should.have.property('message').to.be.a('object');
+    body.should.have.property('trace').eql(null);
+}
+
 describe('POST /events/v1', () => {
     it('publish a new event', (done) => {
-        let body = {
+        let event = {
             "event": "de.ard.eventhub.v1.radio.track.playing",
             "type": "music",
             "start": "2020-01-19T06:00:00+01:00",
@@ -119,13 +123,10 @@ describe('POST /events/v1', () => {
         chai.request(server)
             .post('/events/v1')
             .set('Authorization', 'Bearer ' + accessToken)
-            .send(body)
+            .send(event)
             .end((err, res) => {
                 res.should.have.status(201);
-                res.body.should.be.a('object');
-                res.body.should.have.property('topics').to.be.a('object');
-                res.body.should.have.property('message').to.be.a('object');
-                //res.body.should.have.property('trace').eql(null);
+                testEventKeys(res.body);
             done();
             });
     });
@@ -137,9 +138,31 @@ describe('POST /events/v1', () => {
 
 var subscriptionName;
 
+function testSubscriptionKeys(body) {
+    body.should.be.a('object');
+    body.should.have.property('type').to.be.a('string');
+    body.should.have.property('method').to.be.a('string');
+    body.should.have.property('name').to.be.a('string');
+    body.should.have.property('path').to.be.a('string');
+    body.should.have.property('url').to.be.a('string');
+    body.should.have.property('topic').to.be.a('object');
+    body.topic.should.have.property('name').to.be.a('string');
+    body.topic.should.have.property('path').to.be.a('string');
+    body.should.have.property('ackDeadlineSeconds').to.be.a('number');
+    body.should.have.property('retainAckedMessages').to.be.a('boolean');
+    body.should.have.property('retryPolicy').eql(null);
+    body.should.have.property('serviceAccount').to.be.a('string');
+    body.should.have.property('labels').to.be.a('object');
+    body.labels.should.have.property('id').to.be.a('string');
+    body.labels.should.have.property('organization').to.be.a('string');
+    body.should.have.property('created').to.be.a('string');
+    body.should.have.property('contact').to.be.a('string');
+    body.should.have.property('owner').to.be.a('string');
+}
+
 describe('POST /subscriptions', () => {
     it('add a new subscription to this user', (done) => {
-        let body = {
+        let subscription = {
             "type": "PUBSUB",
             "method": "PUSH",
             "url": "https://example.com/my/webhook/for/this/subscription",
@@ -150,26 +173,10 @@ describe('POST /subscriptions', () => {
         chai.request(server)
             .post('/subscriptions')
             .set('Authorization', 'Bearer ' + accessToken)
-            .send(body)
+            .send(subscription)
             .end((err, res) => {
                 res.should.have.status(201);
-                res.body.should.be.a('object');
-                res.body.should.have.all.keys(
-                    'type', 
-                    'method', 
-                    'name', 
-                    'path', 
-                    'url', 
-                    'topic',
-                    'ackDeadlineSeconds',
-                    'retainAckedMessages',
-                    'retryPolicy',
-                    'serviceAccount',
-                    'labels',
-                    'created',
-                    'contact',
-                    'owner'
-                    )
+                testSubscriptionKeys(res.body);
                 // Store subscription name for further tests
                 subscriptionName = res.body.name;
             done();
@@ -185,22 +192,7 @@ describe('GET /subscriptions', () => {
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
-                res.body.every(i => expect(i).to.have.all.keys(
-                    'type', 
-                    'method', 
-                    'name', 
-                    'path', 
-                    'url', 
-                    'topic',
-                    'ackDeadlineSeconds',
-                    'retainAckedMessages',
-                    'retryPolicy',
-                    'serviceAccount',
-                    'labels',
-                    'created',
-                    'contact',
-                    'owner'
-                    ))
+                res.body.every(i => testSubscriptionKeys(i));
             done();
         });
     });
@@ -213,23 +205,7 @@ describe('GET /subscriptions/{name}', () => {
             .set('Authorization', 'Bearer ' + accessToken)
             .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.all.keys(
-                    'type', 
-                    'method', 
-                    'name', 
-                    'path', 
-                    'url', 
-                    'topic',
-                    'ackDeadlineSeconds',
-                    'retainAckedMessages',
-                    'retryPolicy',
-                    'serviceAccount',
-                    'labels',
-                    'created',
-                    'contact',
-                    'owner'
-                    )
+                testSubscriptionKeys(res.body);
             done();
         });
     });
@@ -244,15 +220,23 @@ describe('DELETE /subscriptions/{name}', () => {
                 res.should.have.status(200);
                 res.body.should.be.a('object');
                 res.body.should.have.property('valid').eql(true);
-                //res.body.should.have.property('trace').eql(null);
+                res.body.should.have.property('trace').eql(null);
             done();
         });
     });
 });
 
-/*
-    TOPICS - Access to topics details
-*/
+// /*
+//     TOPICS - Access to topics details
+// */
+
+function testTopicKeys(body) {
+    body.should.be.a('object');
+    body.should.have.property('type').to.be.a('string');
+    body.should.have.property('name').to.be.a('string');
+    body.should.have.property('path').to.be.a('string');
+    body.should.have.property('labels').to.be.a('object');
+}
 
 describe('GET /topics', () => {
     it('list all available topics', (done) => {
@@ -262,12 +246,7 @@ describe('GET /topics', () => {
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
-                res.body.every(i => expect(i).to.have.all.keys(
-                    'type', 
-                    'name', 
-                    'path', 
-                    'labels'
-                    ))
+                res.body.every(i => testTopicKeys(i));
             done();
         });
     });
