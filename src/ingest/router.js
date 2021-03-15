ed1a09f691dd2be1cf0e0a796ca451d643698851
Daniel Freytag <daniel@frytg.com>
2021-03-15 16:12:57 +0100

    updating router and health checks

--- src/ingest/router.js
+++ src/ingest/router.js
@@ -6,24 +6,30 @@
 */
 
 // import express.router
-const express = require('express');
-const response = require('../utils/response');
-const router = express.Router();
-router.use(express.urlencoded({ extended: true }));
+const express = require('express')
+const moment = require('moment')
+const bodyParser = require('body-parser')
+const OpenApiValidator = require('express-openapi-validator')
+
+// load swagger UI
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../../openapi.json')
+const swaggerConfig = require('../../config/swaggerUI')
+
+// set up router
+const router = express.Router()
+router.use(express.urlencoded({ extended: true }))
 
 // enable body parsing for post requests
-const bodyParser = require('body-parser');
-router.use(bodyParser.json());
+router.use(bodyParser.json())
 
 // load openapi validator
-const OpenApiValidator = require('express-openapi-validator');
-const moment = require('moment');
 router.use(
 	OpenApiValidator.middleware({
 		apiSpec: './openapi.yaml',
 		validateRequests: true,
 		validateResponses: false,
-		ignorePaths: (path) => path.startsWith('/openapi'),
+		ignorePaths: (path) => path.startsWith('/openapi') || path === '/',
 		formats: [
 			{
 				name: 'iso8601-timestamp',
@@ -32,38 +38,41 @@ router.use(
 			},
 		],
 	})
-);
+)
 
-// set openapi error handler
-router.use((err, req, res, next) => {
-	return response.badRequest(req, res, err);
-});
-
-// load swagger UI
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../../openapi.json');
-const swaggerConfig = require('../../config/swaggerUI');
+// load response util
+const response = require('../utils/response')
 
 // register swagger UI endpoint
-router.use('/openapi', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerConfig));
+router.use('/openapi', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerConfig))
 
 // load auth middleware
-const authVerify = require('./auth/middleware/verify');
+const authVerify = require('./auth/middleware/verify')
 
 // register API endpoints
-router.post('/auth/login', require('./auth/login/post'));
-router.post('/auth/refresh', require('./auth/refresh/post'));
-router.post('/auth/reset', require('./auth/reset/post'));
+router.post('/auth/login', require('./auth/login/post'))
+router.post('/auth/refresh', require('./auth/refresh/post'))
+router.post('/auth/reset', require('./auth/reset/post'))
 
-router.post('/events/v1', authVerify, require('./events/post'));
+router.post('/events/v1', authVerify, require('./events/post'))
 
-router.get('/subscriptions/', authVerify, require('./subscriptions/list'));
-router.post('/subscriptions/', authVerify, require('./subscriptions/post'));
-router.get('/subscriptions/:subscriptionName', authVerify, require('./subscriptions/get'));
-router.delete('/subscriptions/:subscriptionName', authVerify, require('./subscriptions/delete'));
+router.get('/subscriptions/', authVerify, require('./subscriptions/list'))
+router.post('/subscriptions/', authVerify, require('./subscriptions/post'))
+router.get('/subscriptions/:subscriptionName', authVerify, require('./subscriptions/get'))
+router.delete('/subscriptions/:subscriptionName', authVerify, require('./subscriptions/delete'))
 
-router.get('/topics/', authVerify, require('./topics/list'));
-router.get('/topics/:topicName', authVerify, require('./topics/list'));
+router.get('/topics/', authVerify, require('./topics/list'))
+router.get('/topics/:topicName', authVerify, require('./topics/list'))
+
+// send healthcheck ok
+router.get(['/', '/health'], (req, res) => {
+	res.sendStatus(200)
+})
+
+// set openapi error handler
+router.use((err, req, res) => {
+	return response.badRequest(req, res, err)
+})
 
 // export router object to server
-module.exports = router;
+module.exports = router
--- src/utils/firebase/verifyToken.js
+++ src/utils/firebase/verifyToken.js
@@ -6,15 +6,16 @@
 */
 
 // load firebase
-var firebaseAdmin = require('firebase-admin');
+const firebaseAdmin = require('firebase-admin')
+
 firebaseAdmin.initializeApp({
 	projectId: process.env.GCP_PROJECT_ID,
-});
+})
 
 module.exports = async (token) => {
 	// attempt to verify token
-	let verification = await firebaseAdmin.auth().verifyIdToken(token);
+	const verification = await firebaseAdmin.auth().verifyIdToken(token)
 
 	// return data
-	return Promise.resolve(verification);
-};
+	return Promise.resolve(verification)
+}
