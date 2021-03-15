2c4eb595e67a5858a62e7b1510c79564c81c7cdc
Daniel Freytag <daniel@frytg.com>
2021-03-15 16:17:59 +0100

    fixing github actions project id

--- src/ingest/auth/reset/post.js
+++ src/ingest/auth/reset/post.js
@@ -6,23 +6,31 @@
 */
 
 // load eventhub utils
-const firebase = require('../../../utils/firebase');
-const response = require('../../../utils/response');
+const firebase = require('../../../utils/firebase')
+const response = require('../../../utils/response')
 
 module.exports = async (req, res) => {
 	try {
 		// try to reset email (may fail if not found)
 		try {
-			let request = await firebase.sendPasswordResetEmail(req.body.email);
+			await firebase.sendPasswordResetEmail(req.body.email)
 		} catch (err) {
+			console.warn(
+				'ingest/auth/reset',
+				'failed resetting password',
+				JSON.stringify({
+					email: req.body.email,
+					err,
+				})
+			)
 			return response.badRequest(req, res, {
 				status: err.error && err.error.code ? err.error.code : 500,
 				data: err,
-			});
+			})
 		}
 
 		// return ok
-		return response.ok(req, res, { valid: true });
+		return response.ok(req, res, { valid: true })
 	} catch (err) {
 		console.error(
 			'ingest/auth/reset',
@@ -31,7 +39,7 @@ module.exports = async (req, res) => {
 				headers: req.headers,
 				error: err.stack || err,
 			})
-		);
-		return response.internalServerError(req, res, err);
+		)
+		return response.internalServerError(req, res, err)
 	}
-};
+}
