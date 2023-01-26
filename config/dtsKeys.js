ceef2ae6743c879367482284b4d1dd1c8bb7ac89
Daniel Freytag <daniel@frytg.com>
2023-01-26 12:42:31 +0100

    fix: new keys in test run

--- config/dtsKeys.js
+++ config/dtsKeys.js
@@ -1,6 +1,6 @@
 // fetch and convert env
-const radiohubKeys = Buffer.from(process.env.DTS_KEYS, 'base64').toString('utf8')
-const decoded = JSON.parse(radiohubKeys)
+const keys = Buffer.from(process.env.DTS_KEYS, 'base64').toString('utf8')
+const decoded = JSON.parse(keys)
 
 // export content
 module.exports = decoded
