c188f6c52b38e35b122800e92ddd30ea853ea569
Daniel Freytag <daniel@frytg.com>
2026-05-12 13:11:50 +0200

    fix: improve error handling for ARD feed download

--- src/data/index.ts
+++ src/data/index.ts
@@ -9,7 +9,6 @@ import type { ArdFeed, ArdLivestream } from '@/types.ard.ts'
 
 const ARD_FEED_URL = getRequiredEnv('ARD_FEED_URL')
 const DOWNLOAD_TO_FILE = false
-const MIN_FEED_PAGE_ITEMS = 251
 const MIN_FEED_ITEMS = 190
 const MAX_FEED_ITEMS = 251
 const STATIONS = [
@@ -25,7 +24,7 @@ const STATIONS = [
 	'hr4',
 ]
 const START_TIME = getMs()
-const source = 'data'
+const source = 'data.getARDFeed'
 
 /**
  * The ARD feed is downloaded and cached in this variable. This is used to avoid multiple downloads of the feed.
@@ -49,24 +48,10 @@ export const getARDFeed = async () => {
 		if (!feedItemCount) return exitWithError('Feed is empty')
 
 		// check if feed has enough items
-		if (feed.pageItemCount < MIN_FEED_PAGE_ITEMS) {
-			const message = `pageItemCount is too small for the ARD Feed with its expected amount of stations: ${feed.pageItemCount}`
-
-			logger.log({
-				level: 'error',
-				message: message,
-				source,
-			})
-
-			return exitWithError(`pageItemCount is too small > ${feed.pageItemCount}`)
-		}
-
-		// check if feed has enough items
 		if (feedItemCount < MIN_FEED_ITEMS) {
-			const message = `ARD Feed contains an unexpected amount of stations: ${feedItemCount}`
 			logger.log({
 				level: 'error',
-				message: message,
+				message: `ARD Feed contains an unexpected amount of stations: ${feedItemCount}`,
 				source,
 			})
 
@@ -86,7 +71,14 @@ export const getARDFeed = async () => {
 		}
 
 		// check for pagination
-		if (feed.totalPageCount > 1) return exitWithError('Pagination is not supported')
+		if (feed.totalPageCount > 1) {
+			logger.log({
+				level: 'error',
+				message: 'Pagination is not supported',
+				source,
+			})
+			return exitWithError('Pagination is not supported')
+		}
 
 		// check if any expected Station is missing
 		for (const station of STATIONS) {
